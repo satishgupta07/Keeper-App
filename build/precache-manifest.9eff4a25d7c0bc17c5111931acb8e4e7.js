b565bbf6bee192e3a2ac2d876ffa83b7b04618d1
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-04 22:05:51 +0530

    make it better
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

---
+++ build/precache-manifest.9eff4a25d7c0bc17c5111931acb8e4e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5171d87acec74ebaa8d16c7ec65945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6e82c5350cfd7d0e745",
+    "url": "/static/js/2.f0775fc7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f0775fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4837e2314e06476268d",
+    "url": "/static/js/main.bcfc4ea1.chunk.js"
+  },
+  {
+    "revision": "b4d12b70b7d90d24ec54",
+    "url": "/static/js/runtime-main.9496a8dc.js"
+  }
+]);
